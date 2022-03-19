48a97949d278448473709b3d35a62be7d573112e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-19 12:23:08 -0400

    chore: chases dependencie renames

--- init.zsh
+++ init.zsh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::p6macosx::deps() {
     ModuleDeps=(
-        p6m7g8/p6common
+        p6m7g8-dotfiles/p6common
     )
 }
 
